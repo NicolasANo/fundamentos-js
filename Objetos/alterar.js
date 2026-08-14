b3f9510ecc66740db5f6ccff729e2fc918b0b85c
NicolasANo <nicolas.nowaski@escola.pr.gov.br>
2026-08-14 11:38:01 +0000

    ALTERAR

---
+++ Objetos/alterar.js
@@ -0,0 +1,12 @@
+const pessoa ={
+    nome: "Nicolas",
+    profissao: "Vagabundo"
+
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "67 222333444";
+
+console.log(pessoa.telefone);
