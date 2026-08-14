524a91e3a2e33028edaead07f82bb23cec7c7cb5
NicolasANo <nicolas.nowaski@escola.pr.gov.br>
2026-08-14 11:02:43 +0000

    acesso

---
+++ Objetos/acesso_colchete.js
@@ -0,0 +1,19 @@
+const cliente ={
+    nome: "Nicolas",
+    idade: 16,
+    cpf:"6767676767",
+    email:"Nicolas@dominio.com"
+};
+
+
+console.log(`0 nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]}anos.`);
+
+
+const chaves = ["nome", "idade", "cpf", "email"];
+
+
+chaves.forEach( (chave) =>  {
+    console.log(`A chave ${chave} tem valor ${cliente[chave]}`);
+})
+
+
